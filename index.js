ef83130c747581f9b18284ab0f19304fbc5229bf
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-05-06 21:42:32 +0700

    ADD | Status For Ping

--- index.js
+++ index.js
@@ -8,18 +8,39 @@ client.once('ready', () =>{
 })
 
 client.on('message', async message => {
-    // console.log(message.content)
     if(message.content.startsWith(`${prefix} kick`)){
-        let member = message.mentions.members.first()
-        member.kick().then((member) => {
-            message.channel.send(":wave: " + member.displayName + " telah di banned!")
-        })
+        if(message.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])){
+            let member = message.mentions.members.first()
+            member.kick().then((member) => {
+                message.channel.send(":wave: " + member.displayName + " telah di banned!")
+            })
+        } else {
+            message.channel.send("Member bngst jangan nglunjak :rage: ")
+        }
     }
 
     if (message.content.startsWith(`ping`)) { 
-        const m = await message.channel.send('Pinging... :bar_chart:');
-        message.channel.send(`Pong! Ping Kamu ${m.createdTimestamp - message.createdTimestamp - Math.round(client.ws.ping)}ms.`)
+        const req = await message.channel.send('Pinging... :bar_chart:');
+        var ping = req.createdTimestamp - message.createdTimestamp - Math.round(client.ws.ping)
+        
+        if(ping < 0)
+            ping *= -1 
+        
+        let msg = ''
+        if (ping > 0 && ping < 50) 
+            msg = `Nasa Speed :rocket:`
+        else if (ping > 50 && ping < 100) 
+            msg = `Wajar Lah :thumbsup:`
+        else if (ping > 100 && ping < 150) 
+            msg = `Ngeleg cok :firecracker:`
+        else if(ping > 150 && ping < 200)
+            msg = `Ampas cok ping-mu :poop:`   
+        else
+            msg = `Pindah kota aja cok :monkey_face:`
+
+        req.edit(`${msg} ${ping}ms.`)
     }
+
 })
 
 client.login(token)
